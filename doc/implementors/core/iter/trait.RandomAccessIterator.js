8903b7dde16d9eefa81131c571775a1a98ce57c5
Steven Fackler <sfackler@gmail.com>
2015-02-28 04:09:10 +0000

    rebuild pages at e24a4ae2ae778d96ae336aec594dc0e3bf95db6e

---
+++ doc/implementors/core/iter/trait.RandomAccessIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['postgres'] = ["<a class='stability Unstable' title='Unstable: not widely used, may be better decomposed into Index and ExactSizeIterator'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.RandomAccessIterator.html' title='core::iter::RandomAccessIterator'>RandomAccessIterator</a> for <a class='struct' href='postgres/struct.RowsIter.html' title='postgres::RowsIter'>RowsIter</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
