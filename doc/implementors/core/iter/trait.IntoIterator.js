606ae96aa322e6a8cbc67c46ca66baa36b8e93b7
Steven Fackler <sfackler@gmail.com>
2015-04-28 06:12:36 +0000

    rebuild pages at 8b6777eee1fee0319985a5d022c61952fe58c289

--- doc/implementors/core/iter/trait.IntoIterator.js
+++ doc/implementors/core/iter/trait.IntoIterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['postgres'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.IntoIterator.html' title='core::iter::IntoIterator'>IntoIterator</a> for &amp;'a <a class='struct' href='postgres/struct.Rows.html' title='postgres::Rows'>Rows</a>&lt;'a&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;'stmt&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.IntoIterator.html' title='core::iter::IntoIterator'>IntoIterator</a> for <a class='struct' href='postgres/struct.Rows.html' title='postgres::Rows'>Rows</a>&lt;'stmt&gt;",];
+implementors['postgres'] = ["impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.IntoIterator.html' title='core::iter::IntoIterator'>IntoIterator</a> for &amp;'a <a class='struct' href='postgres/struct.Rows.html' title='postgres::Rows'>Rows</a>&lt;'a&gt;","impl&lt;'stmt&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.IntoIterator.html' title='core::iter::IntoIterator'>IntoIterator</a> for <a class='struct' href='postgres/struct.Rows.html' title='postgres::Rows'>Rows</a>&lt;'stmt&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
